e50f4df1043ae0e62fdb53ae8d0e656b34174419
gefenbar <gefenbar23@gmail.com>
2024-03-27 14:55:14 +0200

    multiple design changes

--- src/Components/SocialLinks.js
+++ src/Components/SocialLinks.js
@@ -2,12 +2,12 @@ import React from "react";
 export default function SocialLinks() {
   return (
     <div className="side-social-links">
-      <a
+      {/* <a
         href="tel:054-9794-777"
         className={`side-social-link`}
       >
         <img src={"phone.webp"} alt="Phone" />
-      </a>
+      </a> */}
       <a
         href="mailto:orenkl162@gmail.com"
         className={`side-social-link`}
@@ -16,7 +16,7 @@ export default function SocialLinks() {
       </a>
       
       <a
-        href="https://api.whatsapp.com/send?phone=+9725549794777"
+        href="https://wa.me/+972549794777"
         target="_blank"
         rel="noreferrer"
         className={`side-social-link`}
