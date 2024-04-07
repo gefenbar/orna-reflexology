12da91213704e93762a35da4722a13c8837c881e
gefenbar <gefenbar23@gmail.com>
2024-04-07 21:13:31 +0300

    fix

--- src/Components/GiftCardBanner.js
+++ src/Components/GiftCardBanner.js
@@ -1,33 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const EnvelopeAndGiftCard = () => {
   const [isHovered, setIsHovered] = useState(false);
+  const [showEnvelope, setShowEnvelope] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShowEnvelope(window.innerWidth >= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <section className="gift-card-section">
-      <div
-        className="envelope-container"
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        <div className={`envelope ${isHovered ? 'open' : ''}`}>
-          <div className="envelope-flap-container">
-            <div className="envelope-flap" />
-          </div>
-          <div className="envelope-content">
-            <img className="logo" src="logo-full.webp" alt="Logo" />
-            <p>לחצי כאן להזמנת כרטיס מתנה</p>
-          </div>
-          <div className={`gift-card ${isHovered ? 'revealed' : ''}`}>
-            <p>פנקי את עצמך או מישהי מיוחדת בחוויה מפנקת!</p>
-            <button>
-              <Link to="/contact">הזמיני Gift Card</Link>
-            </button>
-            <img className="logo2" src="logo.webp" alt="Logo" />
+      {showEnvelope && (
+        <div
+          className="envelope-container"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <div className={`envelope ${isHovered ? 'open' : ''}`}>
+            <div className="envelope-flap-container">
+              <div className="envelope-flap" />
+            </div>
+            <div className="envelope-content">
+              <img className="logo" src="logo-full.webp" alt="Logo" />
+              <p>לחצי כאן להזמנת כרטיס מתנה</p>
+            </div>
+            <div className={`gift-card ${isHovered ? 'revealed' : ''}`}>
+              <p>פנקי את עצמך או מישהי מיוחדת בחוויה מפנקת!</p>
+              <button>
+                <Link to="/contact">הזמיני Gift Card</Link>
+              </button>
+              <img className="logo2" src="logo.webp" alt="Logo" />
+            </div>
           </div>
         </div>
-      </div>
+      )}
+      {!showEnvelope && (
+        <div className={`gift-card revealed`}>
+          <p>פנקי את עצמך או מישהי מיוחדת בחוויה מפנקת!</p>
+          <button>
+            <Link to="/contact">הזמיני Gift Card</Link>
+          </button>
+          <img className="logo2" src="logo.webp" alt="Logo" />
+        </div>
+      )}
     </section>
   );
 };
