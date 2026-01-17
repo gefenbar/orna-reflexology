c6d0a59119ee9bb971acf01527cf2fd698f8c1be
Gefen <workiza@Workizs-MacBook-Air.local>
2026-01-17 18:57:43 +0200

    sitemap update

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ import HormonalBalance from "./pages/posts/HormonalBalance";
 import HeelSpur from "./pages/posts/HeelSpur";
 import Migraines from "./pages/posts/Migraines";
 import AnxietyRelief from "./pages/posts/AnxietyRelief";
-import Appointment from "./pages/Appointment";
 
 
 function ScrollToTop() {
@@ -70,7 +69,6 @@ export default function App() {
           <Route path="/heel-spur" element={<HeelSpur />} />
           <Route path="/migraines" element={<Migraines />} />
           <Route path="/anxiety-relief" element={<AnxietyRelief />} />
-          <Route path="/appointment" element={<Appointment />} />
         </Routes>
       </BrowserRouter>
       <Footer />
