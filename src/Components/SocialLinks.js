515e6542e7536c4deb0fa195f901590f4c09fbb4
Gefen <workiza@Workizs-MacBook-Air.local>
2026-01-18 16:04:15 +0200

    fix: updated social icons in SocialLinks.js to use root-relative paths

--- src/Components/SocialLinks.js
+++ src/Components/SocialLinks.js
@@ -2,21 +2,21 @@ import React from "react";
 export default function SocialLinks() {
   return (
     <div className="side-social-links">
-   
+
       <a
         href="mailto:orenkl162@gmail.com"
         className={`side-social-link`}
       >
-        <img src={"email.webp"} alt="Email" />
+        <img src={"/email.webp"} alt="Email" />
       </a>
-      
+
       <a
         href="https://wa.me/+972549794777"
         target="_blank"
         rel="noreferrer"
         className={`side-social-link`}
       >
-        <img src={"whatsapp.webp"} alt="WhatsApp" />
+        <img src={"/whatsapp.webp"} alt="WhatsApp" />
       </a>
       <a
         href="https://www.instagram.com/nissaniklein"
@@ -24,7 +24,7 @@ export default function SocialLinks() {
         rel="noreferrer"
         className={`side-social-link`}
       >
-        <img src={"instagram.webp"} alt="Instagram" />
+        <img src={"/instagram.webp"} alt="Instagram" />
       </a>
     </div>
   );
